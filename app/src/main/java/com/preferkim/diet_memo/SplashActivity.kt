5ad560e5a5a004df649afed5dbc12d92161254de
Balparang <shineecard5@gmail.com>
2022-04-25 15:26:34 +0900

    [추가] SplashActivity: 비회원 로그인 기능 추가

--- app/src/main/java/com/preferkim/diet_memo/SplashActivity.kt
+++ app/src/main/java/com/preferkim/diet_memo/SplashActivity.kt
@@ -4,18 +4,62 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import java.lang.Exception
 
 class SplashActivity : AppCompatActivity() {
+
+    lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }, 3000)
+        firebaseAuth = FirebaseAuth.getInstance() // auth = Firebase.auth 대체
+
+        try {
+
+            Log.d("splash", firebaseAuth.currentUser!!.uid)
+
+            Toast.makeText(
+                this, "원래 비회원 로그인 되어있는 사람입니다.",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            Handler().postDelayed({
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }, 3000)
+
+        } catch (e: Exception) {
+
+            Log.d("splash", "회원가입 시켜줘야함")
+
+            firebaseAuth.signInAnonymously()
+                .addOnSuccessListener {
+                    Toast.makeText(
+                        this, "익명 로그인 성공",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    Handler().postDelayed({
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    }, 3000)
+                }
+                .addOnFailureListener {
+                    Toast.makeText(
+                        this, "익명 로그인 실패",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+        }
+
+
 
     }
 }
