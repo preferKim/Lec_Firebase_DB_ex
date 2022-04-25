f68c855415d96c5c94db1aa05e524f2aee8a52df
Balparang <shineecard5@gmail.com>
2022-04-25 16:12:04 +0900

    [추가] custom_dialog, date picker dialog

--- app/src/main/java/com/preferkim/diet_memo/MainActivity.kt
+++ app/src/main/java/com/preferkim/diet_memo/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.preferkim.diet_memo
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val writeBtn = findViewById<ImageView>(R.id.writeBtn)
+        writeBtn.setOnClickListener {
+            // custom dialog 띄우기
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.custom_dialog, null)
+            val mBuilder = AlertDialog.Builder(this).setView(mDialogView).setTitle("운동 메모 다이얼로그")
+
+            val mAlertDialog = mBuilder.show()
+
+            val dateSelectBtn = mAlertDialog.findViewById<Button>(R.id.dateSelectBtn)
+
+            dateSelectBtn?.setOnClickListener {
+
+                val today = GregorianCalendar()
+                val year: Int = today.get(Calendar.YEAR)
+                val month: Int = today.get(Calendar.MONTH)
+                val date: Int = today.get(Calendar.DATE)
+
+                val dlg = DatePickerDialog(this, object : DatePickerDialog.OnDateSetListener {
+                    override fun onDateSet(
+                        view: DatePicker?,
+                        year: Int,
+                        month: Int,
+                        dayOfMonth: Int
+                    ) {
+                        dateSelectBtn.setText("${year}, ${month}, ${dayOfMonth}")
+                    }
+
+                }, year, month, date)
+
+                dlg.show()
+
+            }
+
+        }
+
+
     }
+
 }
